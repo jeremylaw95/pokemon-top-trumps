0b509828067bfa6d305e1b5a1efea484e7d15735
Matthew Carless <matthewcarless@Matthews-MacBook-Pro.local>
2020-10-02 11:55:04 +0100

    second commit

--- main.js
+++ main.js
@@ -3,20 +3,31 @@ let fetchButton = document.querySelector("#fetch-button")
 fetchButton.addEventListener("click", getData)
 
 function randNumGen() {
-    let randNum = Math.floor(Math.random()*10) + 1
+    let randNum = Math.floor(Math.random() * 10) + 1
     return randNum
 }
+
+let playerDeck = document.querySelector("#player-deck");
+let computerDeck = document.querySelector("#computer-deck");
+
 
 // randNumGen()
 // console.log(randNum)
 
 async function getData() {
-    
+
     let fetchRequest = await fetch(`https://pokeapi.co/api/v2/pokemon/${randNumGen()}`)
-    
+
     let data = await fetchRequest.json()
-    let {name, height, weight, base_experience} = data
-    console.log({name, height, weight, base_experience})
+    let { name, height, weight, base_experience } = data
+    console.log({ name, height, weight, base_experience })
+}
+
+
+function createDecks() {
+    for(let i=0;i < 5; i++) {
+        let newLI = document.createElement("li");   
+    }
 }
 
 // create divs in html and set inline with width 50%
